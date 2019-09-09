ede2f550f71f784211107743308724b2a3acef78
Ana Kely Sobrino de Sousa <2017118050384@ifpi.int>
2019-09-09 08:24:08 -0300

    Ajuste no endpoint categorias/{id}

--- src/main/java/com/ifpi/ted2019/services/CategoriaService.java
+++ src/main/java/com/ifpi/ted2019/services/CategoriaService.java
@@ -1,18 +1,25 @@
 package com.ifpi.ted2019.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ifpi.ted2019.domain.Categoria;
 import com.ifpi.ted2019.repositories.CategoriaRepository;
+import com.ifpi.ted2019.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria buscar(Integer id) {
-		return repository.findById(id).orElse(null);
+		Optional<Categoria> object = repository.findById(id);
+
+		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
 }
